5e3c26561d7931b7e4c4b9949d1957f306c5ac75
QSP <QSP@QSP>
2021-04-29 08:03:55 +0530

    Hi

---
+++ src/test/java/scripts/Test3.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void demo() {
+		Reporter.log("Hello python",true);
+	}
+
+}
